cdc583c3efab0ae90ec47d206fefb1795cab090d
mbp16 <phw1508@naver.com>
2023-12-22 00:02:09 +0800

    test complete

--- local/core/robot_control.py
+++ local/core/robot_control.py
@@ -1,19 +1,30 @@
 import Arm_Lib
 import json
+import time
 
 
 class RobotControl:
     def __init__(self):
-        self.coordinates = json.load(open('../coordinates.json'))
+        self.coordinates = json.load(open('local/coordinates.json'))
         self.standby_number = 1
         self.robot = Arm_Lib.Arm_Device()
         pass
 
     def move(self, number: int):
         self.robot.Arm_serial_servo_write6(*self.coordinates["standby" + str(self.standby_number)], 180, 600)
+        time.sleep(0.6)
         self.robot.Arm_serial_servo_write6(*self.coordinates["standby" + str(self.standby_number)], 0, 600)
+        time.sleep(0.6)
         self.robot.Arm_serial_servo_write6(*self.coordinates["board" + str(number)], 0, 600)
+        time.sleep(0.6)
         self.robot.Arm_serial_servo_write6(*self.coordinates["board" + str(number)], 180, 600)
+        time.sleep(0.6)
         self.robot.Arm_serial_servo_write6(90, 90, 90, 90, 90, 0, 600)
+        time.sleep(0.6)
         self.standby_number += 1
         pass
+
+
+if __name__ == '__main__':
+    control = RobotControl()
+    control.move(1)
